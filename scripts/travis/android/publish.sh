afd1ce1e4e00ca894570a770364d46ee26b8e5b0
abushmin <diskbu@yandex.ru>
2020-07-05 16:54:53 +0300

    fix publish script

--- scripts/travis/android/publish.sh
+++ scripts/travis/android/publish.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-export TERM=dumb
-
-export API=$ANDROID_API
-export BUILD_TOOLS_VERSION=$ANDROID_BUILD_TOOLS_VERSION
-export NDK_VERSION=$ANDROID_NDK_VERSION
-
-cd android
-./gradlew -x build bintrayUpload
+docker run -t --rm --privileged --device /dev/kvm \
+    --env API=$ANDROID_API \
+    --env BUILD_TOOLS_VERSION=$ANDROID_BUILD_TOOLS_VERSION \
+    --env NDK_VERSION=$ANDROID_NDK_VERSION \
+    --env ROC_BASE_DIR=$ROC_BASE_DIR \
+    --env BINTRAY_USER=$BINTRAY_USER \
+    --env BINTRAY_KEY=$BINTRAY_KEY \
+    --env BINTRAY_REPO=$BINTRAY_REPO \
+    --env BINTRAY_REPO_ORG=$BINTRAY_REPO_ORG \
+    -v "${PWD}:${PWD}" -v $ROC_BASE_DIR:$ROC_BASE_DIR \
+    -v android-sdk:/sdk -w "${PWD}" \
+        rocproject/java-android:jdk$JAVA_VERSION \
+            $PWD/scripts/travis/android/entrypoint/publish.sh
